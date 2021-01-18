d2d0321cde1eeac079ab21d401bbff5fdb465a50
SaiRagh4 <smaddhur@purdue.edu>
2021-01-18 18:22:32 -0500

    more updates

--- test.py
+++ test.py
@@ -3,51 +3,36 @@ from torch import nn, optim
 from torch.utils.data.sampler import SubsetRandomSampler
 from torch.autograd.variable import Variable
 import os
-from tqdm import tqdm
 import numpy as np
 import argparse
-import calendar, time
 
-from logger import Logger
 from model import Generator, Discriminator
 import utils
 from utils import TrainDatasetFromFolder as TDF
-from loss import GeneratorLoss
-
-
 
 def main():
 	parser = argparse.ArgumentParser()
 	parser.add_argument("--G_path", help="Generator mdoel path")
 	parser.add_argument("--D_path", help="Discriminator mdoel path")
-	parser.add_argument("--vis_path", help="Store SR images path")
+	parser.add_argument("--dir_path", help="path to load LR images and store SR images")
 	parser.add_argument("--batch_size", type=int, help="Batch size")
 	parser.add_argument("--res_blocks", type=int, help="No. of resnet blocks")
 	parser.add_argument("--in_channels", type=int, help="No. of input channels")
-	parser.add_argument("--dropout", type=float, help="Dropout")
 	parser.add_argument("--train", type=int, help="Train - 1 or Test - 0")
 	parser.add_argument("--downsample", nargs='?', const=True, default=False, help="Downsampling GAN")
-	parser.add_argument("--loss", help="Loss function - mse/bce/comb")
-	parser.add_argument("--norm", help="BN or IN for Generator")
 
 	args = parser.parse_args()
-	print(args.batch_size)
-	print(args.res_blocks)
-	print(args.in_channels)
 
 
 	pathG=args.G_path
 	pathD=args.D_path
-	srDir=args.vis_path
+	srDir=args.dir_path
 
 	test_batch_size = args.batch_size
 	shuffle_dataset = True
 	random_seed= 42
 
-	# Creating data indices for training and validation splits:
-	# dataset_size = len(data_loader)*batch_size
-	# root='/net/kihara/scratch/smaddhur/SuperReso/dataset_clean'
-	root='/net/kihara-fast-scratch/smaddhur/SuperReso/EM_data/dataset_new_2_6'
+	root=srDir
 	dataset = TDF(root,25,2,args.downsample,args.train) 
 	dataset_size = len(dataset)
 	print(dataset_size)
@@ -56,7 +41,6 @@ def main():
 	    np.random.seed(random_seed)
 	    np.random.shuffle(indices)
 
-	# indices_test=indices[900000:900160]
 	indices_test=indices
 
 	print(len(indices_test))
@@ -70,17 +54,15 @@ def main():
 	num_batches=len(test_loader)
 	print(num_batches)
 
-	G = Generator(args.in_channels,2,args.res_blocks, args.downsample, args.dropout, args.norm)
+	G = Generator(args.in_channels,2,args.res_blocks, args.downsample)
 	G.load_state_dict(torch.load(pathG))
 	G.eval()
-	D = Discriminator(args.in_channels, args.dropout)
+	D = Discriminator(args.in_channels)
 	D.load_state_dict(torch.load(pathD))
 	D.eval()
 
 	G.cuda()
 	D.cuda()
-
-	G_criterionLoss = GeneratorLoss(args.loss).cuda()
 
 	coords=["0","0","0"]
 	with torch.no_grad():
@@ -95,20 +77,11 @@ def main():
 			hr_test=D(val_target).mean()
 			hr_fake=D(sr_test).mean()
 
-			G_loss_test=G_criterionLoss(hr_fake, sr_test, val_target)
-			D_loss_test=1 - hr_test + hr_fake
 			utils.write_voxels(args.batch_size, srDir, sr_test, index, args.downsample, "test",coords,filName)
 			if (index) % 50 == 0: 
 				print(index)
 
 		print(torch.cuda.memory_allocated())
-		print('TEST - ')
-		print("D_loss:%d,G_loss:%",(D_loss_test, G_loss_test))
-
-		del G_loss_test
-		del D_loss_test
-
-
 
 if __name__=="__main__":
 	main()
--- utils.py
+++ utils.py
@@ -2,9 +2,9 @@ import torch
 from torch import nn
 from torch.utils.data.dataset import Dataset
 from torch.autograd.variable import Variable
-from torchvision import transforms
-from skimage.transform import resize
-from PIL import Image
+# from torchvision import transforms
+# from skimage.transform import resize
+# from PIL import Image
 import os
 import numpy as np
 
@@ -68,21 +68,18 @@ def pixel_shuffle(input,up_scale):
     shuffle_out = input_view.permute(0, 1, *(indicies[::-1])).contiguous()
     return shuffle_out.view(input_size[0], input_size[1], *output_size)
 
-def train_hr_transform():
-    return transforms.Compose([
-        # transforms.RandomCrop(crop_size),
-        transforms.ToTensor(),
-])
-def train_lr_transform(im_size, upscale_factor):
+# def train_hr_transform():
+#     return transforms.Compose([
+#         # transforms.RandomCrop(crop_size),
+#         transforms.ToTensor(),
+# ])
+# def train_lr_transform(im_size, upscale_factor):
 
-    return transforms.Compose([
-        transforms.ToPILImage(),
-        transforms.Resize(im_size // upscale_factor, interpolation=Image.BICUBIC),
-        transforms.ToTensor()
-])
-
-train_set=['6123', '6351', '6481', '6535', '6551', '6526', '6337', '3056', '3179', '3180', '3238', '3337', '3331', '8013', '9537', '9566', '6656', '6668', '6618', '8267', '8289', '4037', '4038', '4040', '4054', '4062', '4071', '4074', '4076', '4077', '4079', '4080', '4083', '4088', '4098', '4138', '3447', '3448', '3524', '3545', '3583', '3601', '3602', '3605', '3654', '3696', '8640', '3727', '3773', '3802', '3776', '8331', '8354', '8398', '8399', '8436', '8454', '8467', '8479', '8481', '8540', '8584', '8585', '8586', '8595', '8608', '8621', '8658', '8702', '8717', '8744', '8745', '8746', '8751', '8794', '8795', '8823', '8840', '6675', '6679', '6700', '6710', '6732', '6733', '6734', '6774']
-test_set=["3388","8750","6479","8511","6714","8409","2788","3790","8728","3672","2484","6677","5779","8643","8644","8188","8242","8015","8778","5623","4128","8001","3531","8624","6711","4061","6344","3366","6441","8004","8148","8641","3378","6489","4075","8581","8771","4078","3662","3663"]
+#     return transforms.Compose([
+#         transforms.ToPILImage(),
+#         transforms.Resize(im_size // upscale_factor, interpolation=Image.BICUBIC),
+#         transforms.ToTensor()
+# ])
 
 class TrainDatasetFromFolder(Dataset):
     def __init__(self, dataset_dir, im_size, upscale_factor=2, downsample=False,mode=1):
@@ -90,24 +87,16 @@ class TrainDatasetFromFolder(Dataset):
         self.im_size=im_size
         self.upscale_factor=upscale_factor
         self.downsample=downsample
-        if mode==1  :
-            dlist = train_set
-        else:
-            dlist = test_set
         if self.downsample:
-            self.hr_imageFileNames = [os.path.join(dataset_dir, x) for x in os.listdir(dataset_dir) if "_LR" in x and x.strip().split("_")[0] in dlist]
+            self.hr_imageFileNames = [os.path.join(dataset_dir, x) for x in os.listdir(dataset_dir) if "_LR" in x and x.strip().split("_")[0]]
             self.lr_transform = train_lr_transform(im_size, upscale_factor)
             self.hr_transform = train_hr_transform()
         else:
-            self.lr_imageFileNames = [os.path.join(dataset_dir, x) for x in os.listdir(dataset_dir) if "_LR" in x and x.strip().split("_")[0] in dlist]
-            self.hr_imageFileNames = [os.path.join(dataset_dir, x) for x in os.listdir(dataset_dir) if "_HR" in x and x.strip().split("_")[0] in dlist]
+            self.lr_imageFileNames = [os.path.join(dataset_dir, x) for x in os.listdir(dataset_dir) if "_LR" in x and x.strip().split("_")[0]]
+            self.hr_imageFileNames = [os.path.join(dataset_dir, x) for x in os.listdir(dataset_dir) if "_HR" in x and x.strip().split("_")[0]]
             self.lr_imageFileNames.sort()
             self.hr_imageFileNames.sort()
 
-        print(len(self.hr_imageFileNames))
-        print(len(self.lr_imageFileNames))
-        print(self.hr_imageFileNames[50001:50010])
-        print(self.lr_imageFileNames[50001:50010])
 
     def __getitem__(self,index):
         hr_filename = self.hr_imageFileNames[index]
@@ -117,7 +106,7 @@ class TrainDatasetFromFolder(Dataset):
         # hr_image = self.hr_transform(hr_image[0])
         # print(hr_image[0])
         if self.downsample:
-            lr_image = resize(hr_image[0],(self.im_size // self.upscale_factor,self.im_size // self.upscale_factor,self.im_size // self.upscale_factor),order=3,preserve_range=True)
+            # lr_image = resize(hr_image[0],(self.im_size // self.upscale_factor,self.im_size // self.upscale_factor,self.im_size // self.upscale_factor),order=3,preserve_range=True)
             # print(lr_image.shape)
             lr_image = np.array([lr_image])
             # lr_image = self.lr_transform(hr_image[0])
